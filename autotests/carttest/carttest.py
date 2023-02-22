f5202529ff39982a82c85bab4af4a6388cc113d6
yurak <yurakotsur03@gmail.com>
2023-02-22 16:44:57 +0200

    0.0.3

--- autotests/carttest/carttest.py
+++ autotests/carttest/carttest.py
@@ -6,27 +6,33 @@ import time
 
 class cart_test(unittest.TestCase):
     def setUp(self):
+
         self.driver = webdriver.Chrome()
+
+    def login(self):
+        driver = self.driver
+        driver.get('https://www.saucedemo.com/')
+        login_field = driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys('standard_user')
+        password_field = driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("secret_sauce")
+        login_button = driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
 
     def test_add_to_cart(self):
         driver = self.driver
-        driver.get('https://www.saucedemo.com/')
-        login_field = driver.find_element(By.XPATH, '//*[@id="user-name"]')
-        login_field.send_keys('standard_user')
-        password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
-        password_field.send_keys("secret_sauce")
-        login_button = driver.find_element(By.XPATH, '//*[@id="login-button"]')
-        login_button.click()
-        add_to_cart_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')
-        add_to_cart_button.click()
+        self.login()
+        add_to_cart_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
         time.sleep(1)
-        cart = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]')
-        cart.click()
+        cart = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]').click()
         time.sleep(1)
         cart_item = driver.find_element(By.CLASS_NAME, 'cart_item')
         self.assertTrue(cart_item.is_displayed())
 
-
+    def test_review_correct_item_added_to_cart(self):
+        self.login()
+        driver = self.driver
+        correct_item = driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div').text
+        add_to_cart_button = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+        cart = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]').click()
+        self.assertTrue(correct_item in driver.page_source)
 
 
     def tearDown(self):
