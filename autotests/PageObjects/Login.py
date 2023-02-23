faa4cde6b5aa0fd31252abcac7950ed049b085bb
yurak <yurakotsur03@gmail.com>
2023-02-23 17:54:55 +0200

    0.0.8

--- autotests/PageObjects/Login.py
+++ autotests/PageObjects/Login.py
@@ -12,11 +12,7 @@ class Login:
         self.password_field = (By.XPATH, '//*[@id="password"]')
         self.login_button = (By.XPATH, '//*[@id="login-button"]')
 
-    def enter_username(self, username):
+    def login(self, username, password):
         self.wait.until(EC.visibility_of_element_located(self.username_field)).send_keys(username)
-
-    def enter_password(self, password):
         self.wait.until(EC.visibility_of_element_located(self.password_field)).send_keys(password)
-
-    def button_click(self):
         self.wait.until(EC.visibility_of_element_located(self.login_button)).click()
--- autotests/login/logintest.py
+++ autotests/login/logintest.py
@@ -1,5 +1,7 @@
 import unittest
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from PageObjects.Login import Login
 
 
@@ -11,9 +13,15 @@ class SaucedemoLogin(unittest.TestCase):
         self.login_page = Login(self.driver)
 
     def test_login_valid_credentials(self):
-        self.login_page.enter_username('standard_user')
-        self.login_page.enter_password('secret_sauce')
-        self.login_page.button_click()
+        driver = self.driver
+        self.login_page.login('standard_user', 'secret_sauce')
+        title = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
+        self.assertTrue(title.is_displayed())
+
+    def test_login_invalid_credentials(self):
+        self.login_page.login('standard_user', 'wrongpass')
+        error = self.driver.find_element(By.XPATH, '//*[@id="login_button_container"]/div/form/div[3]/h3')
+        self.assertTrue(error.is_displayed())
 
     def tearDown(self):
         self.driver.quit()
